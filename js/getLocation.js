28e01657f6ddca9901ac07340665312d1307bdd5
HarleyMartins <harleyfla05@gmail.com>
2024-10-16 12:35:16 -0300

    feat: initial version

---
+++ js/getLocation.js
@@ -0,0 +1,4 @@
+function getLocation() {
+  window.location.href =
+    "https://www.google.com.br/maps/place/Escola+Estadual+de+Educação+Profissional+Manuel+Abdias+Evangelista/@-4.7062813,-40.5549968,847m/data=!3m1!1e3!4m6!3m5!1s0x795cd55d72c9393:0x5f0ddc114cdc751e!8m2!3d-4.7068169!4d-40.5547197!16s%2Fg%2F1pztv9x92?entry=ttu&g_ep=EgoyMDI0MTAxMy4wIKXMDSoASAFQAw%3D%3D"
+}
